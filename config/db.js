afd2d71275605169a59fff779c3a9066663f9a65
parth9218 <33871605+parth9218@users.noreply.github.com>
2019-02-25 13:39:59 +0530

    Added Parth chauhan's db url for test

--- config/db.js
+++ config/db.js
@@ -1,1 +1,2 @@
-module.exports.url = process.env.DB_URL || 'postgres://rjakaexw:xD-tGC7Up4TvS-24iTBVRnCKtjrYEdF2@baasu.db.elephantsql.com:5432/rjakaexw';
+module.exports.url = process.env.DB_URL || 'postgres://spcvnsdk:iyztoE1yyYGSbOX-630H1q2nZZ5v5KXI@baasu.db.elephantsql.com:5432/spcvnsdk';
+// postgres://rjakaexw:xD-tGC7Up4TvS-24iTBVRnCKtjrYEdF2@baasu.db.elephantsql.com:5432/rjakaexw
