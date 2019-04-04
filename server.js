fc289b351061d5825296e52191cc5f0674fbd7f9
Parth <prajapatiparth06@gmail.com>
2019-04-04 13:30:02 +0530

    Cors domain add

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.all('*', (req, res, next) => {
 	// CORS headers
 	res.header(
 		'Access-Control-Allow-Origin',
-		security.getAccessControlAllowOrigin()
+		'https://viral123.herokuapp.com'
 	);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header('Access-Control-Allow-Credentials', 'true');
